0cdf8b5293b7b87345117e7f0b3f76fd196b9354
vetrivel-1 <vetrivelarumgam95@gmail.com>
2023-03-20 17:03:53 +0530

    bankprogram

--- Usebank.java
+++ Usebank.java
@@ -3,7 +3,7 @@ package com.oneoft.day17;
 public class Usebank {
 	public static void main(String[]args) {
 		PrivateBank pb=new PrivateBank ();
-		System.out.println(pb.PrincipleAmount(2000));
+		System.out.println(pb.PrincipleAmount(10));
 	}
 
 }
